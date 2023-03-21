57cf7956d358aefe9499bbeefba40a8eb2119723
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-03-20 19:30:31 -0500

    fix: error en register user y feat: obtener roles

--- view/Registropersona_prueba.cs
+++ view/Registropersona_prueba.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using control;
+using model;
 
 namespace view
 {
@@ -25,7 +26,7 @@ namespace view
 
         private async void btnGuardar_Click(object sender, EventArgs e)
         {
-            string sexoE="";
+            string sexoE = "";
             if (rbtnSexoM.Checked)
             {
                 sexoE = rbtnSexoM.Text;
@@ -44,6 +45,17 @@ namespace view
             {
                 MessageBox.Show("Error en Registro!");
             }
+        }
+
+        private async void Registropersonal_prueba_Load(object sender, EventArgs e)
+        {
+            List<RolUsuario> roles = new List<RolUsuario>();
+            RolUsuarioCtrl rolUsuarioCtrl = new RolUsuarioCtrl();
+            roles = await rolUsuarioCtrl.ObtenerRolesCtrl();
+            cmbCargo.DataSource = roles;
+            cmbCargo.DisplayMember = "Nombre";
+            cmbCargo.ValueMember = "Id";
+            cmbCargo.SelectedValue = -1;
         }
     }
 }

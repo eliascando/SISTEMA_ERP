8aeb9dbf6f34171ee43c0f800ee3e2e4315a7a2c
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-03-23 15:57:39 -0500

    update

--- view/Program.cs
+++ view/Program.cs
@@ -11,7 +11,7 @@ namespace view
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new VentanaPrincipal_prueba());
+            Application.Run(new InicioSesion_prueba());
         }
     }
 }

fc18da9e2ee58c0aed7b6f71d4379f019acd44de
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-04-01 08:38:39 -0500

    update

--- aurora/Aurora.cs
+++ aurora/Aurora.cs
@@ -171,28 +171,5 @@ namespace aurora
                 return false;
             }
         }
-        public static string DecryptConnectionString(string connectionString)
-        {
-            string encryptionKey = "elin";
-            connectionString = connectionString.Replace(" ", "+");
-            byte[] cipherBytes = Convert.FromBase64String(connectionString);
-            using (Aes encryptor = Aes.Create())
-            {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(cipherBytes, 0, cipherBytes.Length);
-                        cs.Close();
-                    }
-                    connectionString = Encoding.Unicode.GetString(ms.ToArray());
-                }
-            }
-            return connectionString;
-        }
-
     }
 }
--- model/Data/ConexionBD.cs
+++ model/Data/ConexionBD.cs
@@ -1,6 +1,9 @@
 ﻿using System.Data.SqlClient;
 using System.Configuration;
 using aurora;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace model.Data
 {
     public class ConexionBD
@@ -11,9 +14,7 @@ namespace model.Data
             try
             {
                 cn = new SqlConnection();
-                string connectionString = ConfigurationManager.ConnectionStrings["conexionsql"].ConnectionString;
-                string decryptedConnection = Aurora.DecryptConnectionString(connectionString);
-                cn.ConnectionString = decryptedConnection;
+                cn.ConnectionString = DecryptConnectionString(ConfigurationManager.ConnectionStrings["conexionsql"].ConnectionString);
                 await cn.OpenAsync();
                 return true;
             }
@@ -36,6 +37,28 @@ namespace model.Data
             {
                 MessageBox.Show("ERROR DE EXCEPCIÓN!: " + ex.Message);
             }
+        }
+        internal static string DecryptConnectionString(string connectionString)
+        {
+            string encryptionKey = "elin";
+            connectionString = connectionString.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(connectionString);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    connectionString = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return connectionString;
         }
     }
 }

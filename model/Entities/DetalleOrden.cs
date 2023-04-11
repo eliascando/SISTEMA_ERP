0e8f2f1da26cfa1a8ab21a67d9ea8640ab66e523
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-04-11 12:29:58 -0500

    feat: clases orden de compra y detalle de orden

---
+++ model/Entities/DetalleOrden.cs
@@ -0,0 +1,40 @@
+﻿namespace model.Entities
+{
+    public class DetalleOrden
+    {
+        private int id_orden;
+        private string numero_orden;
+        private string codigo_producto;
+        private int cantidad_producto;
+        private double costo_unitario;
+        private double iva_producto;
+        private double descuento_producto;
+        private double total_producto;
+
+        public DetalleOrden()
+        {
+
+        }
+
+        public DetalleOrden(int id_orden, string numero_orden, string codigo_producto, int cantidad_producto, double costo_unitario, double iva_producto, double descuento_producto, double total_producto)
+        {
+            Id_orden = id_orden;
+            Numero_orden = numero_orden;
+            Codigo_producto = codigo_producto;
+            Cantidad_producto = cantidad_producto;
+            Costo_unitario = costo_unitario;
+            Iva_producto = iva_producto;
+            Descuento_producto = descuento_producto;
+            Total_producto = total_producto;
+        }
+
+        public int Id_orden { get => id_orden; set => id_orden = value; }
+        public string Numero_orden { get => numero_orden; set => numero_orden = value; }
+        public string Codigo_producto { get => codigo_producto; set => codigo_producto = value; }
+        public int Cantidad_producto { get => cantidad_producto; set => cantidad_producto = value; }
+        public double Costo_unitario { get => costo_unitario; set => costo_unitario = value; }
+        public double Iva_producto { get => iva_producto; set => iva_producto = value; }
+        public double Descuento_producto { get => descuento_producto; set => descuento_producto = value; }
+        public double Total_producto { get => total_producto; set => total_producto = value; }
+    }
+}
---
+++ model/Entities/OrdenCompra.cs
@@ -0,0 +1,43 @@
+﻿namespace model.Entities
+{
+    public class OrdenCompra
+    {
+        private string numero_orden;
+        private string id_proveedor;
+        private string id_establecimiento;
+        private string id_usuario;
+        private DateTime fechayhora;
+        private double subtotal;
+        private double descuento_total;
+        private double iva_total;
+        private double valor_total;
+
+        public OrdenCompra()
+        {
+
+        }
+
+        public OrdenCompra(string numero_orden, string id_proveedor, string id_establecimiento, string id_usuario, DateTime fechayhora, double subtotal, double descuento_total, double iva_total, double valor_total)
+        {
+            Numero_orden = numero_orden;
+            Id_proveedor = id_proveedor;
+            Id_establecimiento = id_establecimiento;
+            Id_usuario = id_usuario;
+            Fechayhora = fechayhora;
+            Subtotal = subtotal;
+            Descuento_total = descuento_total;
+            Iva_total = iva_total;
+            Valor_total = valor_total;
+        }
+
+        public string Numero_orden { get => numero_orden; set => numero_orden = value; }
+        public string Id_proveedor { get => id_proveedor; set => id_proveedor = value; }
+        public string Id_establecimiento { get => id_establecimiento; set => id_establecimiento = value; }
+        public string Id_usuario { get => id_usuario; set => id_usuario = value; }
+        public DateTime Fechayhora { get => fechayhora; set => fechayhora = value; }
+        public double Subtotal { get => subtotal; set => subtotal = value; }
+        public double Descuento_total { get => descuento_total; set => descuento_total = value; }
+        public double Iva_total { get => iva_total; set => iva_total = value; }
+        public double Valor_total { get => valor_total; set => valor_total = value; }
+    }
+}
